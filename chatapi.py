909ec6b228d0873032911fd42d8da9eb40cdcaa1
Sarah Holderness <sarah@Joy.local>
2025-02-07 18:33:04 -0500

    Module 1 Complete

---
+++ chatapi.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+my_api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key = my_api_key)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "system", 
+            "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about a cold day."
+        }
+    ]
+)
+
+print("response: ", completion.choices[0].message)
