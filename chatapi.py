df7b67c137b5497d0cf428e3efa326b622d7d49a
Sarah Holderness <sarah@Joy.local>
2025-02-09 11:26:50 -0500

    Module 2 changes

--- chatapi.py
+++ chatapi.py
@@ -1,23 +1,38 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 load_dotenv()
 my_api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key = my_api_key)
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "system", 
-            "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about a cold day."
-        }
-    ]
-)
+class ChatRequest(BaseModel):
+    prompt: str
 
-print("response: ", completion.choices[0].message)
+class ChatResponse(BaseModel):
+    response: str
+
+app = FastAPI()
+
+
+@app.post("/")
+def ai_prompt(request: ChatRequest):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system", 
+                "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": request.prompt
+            }
+        ]
+    )
+
+    gpt_response = completion.choices[0].message.content
+    return ChatResponse(response = gpt_response)
+
