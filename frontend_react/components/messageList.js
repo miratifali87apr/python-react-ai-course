6018ef0827a43f4229c02c036efd12b9efc43616
Sarah Holderness <s.b.holderness@gmail.com>
2025-03-28 14:05:41 -0400

    Update messageList.js

--- frontend_react/components/messageList.js
+++ frontend_react/components/messageList.js
@@ -19,7 +19,7 @@ const MessageList = () => {
         formData.append("file", imageFile);
       }
 
-      const response = await fetch("http://python-react-ai-course.onrender.com/uploadfile/", {
+      const response = await fetch("https://python-react-ai-course.onrender.com/uploadfile/", {
         method: "POST",
         body: formData,
       });
